522e848e61e11f03bf1bb386054cea4b469dd229
72 <72sevenzy2@gmail.com>
2026-09-21 18:42:49 +0500

    test: write test for .readStream() and Read().

--- storage_test.go
+++ storage_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"io"
 	"testing"
 )
 
@@ -10,8 +11,19 @@ func TestStorage(t *testing.T) {
 		PathTransformFunc: TransformPathFunc,
 	}
 	store := NewStorage(storeOps)
+	key := "somekey"
+	bytesData := []byte("some jpeg")
 
-	if err := store.writeToStream("somekey", bytes.NewReader([]byte("some jpeg"))); err != nil {
+	if err := store.writeToStream(key, bytes.NewReader(bytesData)); err != nil {
 		t.Error(err)
+	}
+
+	r, err := store.Read(key)
+	if err != nil {
+		t.Error(err)
+	}
+	b, _ := io.ReadAll(r)
+	if string(b) != string(bytesData) {
+		t.Errorf("invalid bytes read, have %s, want %s", string(b), string(bytesData))
 	}
 }
